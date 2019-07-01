6b3d10763a9691f5d66b7ddaf47e8711864fe1db
Jorge Miranda <jorgemirandaneto@gmail.com>
2019-06-30 22:15:34 -0300

    Atualizacao

--- Comum/SendEmail.cs
+++ Comum/SendEmail.cs
@@ -1,31 +1,28 @@
-using MailKit.Net.Smtp;
-using MimeKit;
-
 namespace Fantasy_server.Comum
 {
     public class SendEmail
     {
          public static void Mail () {
-            MimeMessage mensage = new MimeMessage ();
+            // MimeMessage mensage = new MimeMessage ();
 
-            mensage.From.Add (new MailboxAddress ("Jorge", "jorgemirandaneto@gmail.com"));
-            mensage.To.Add (new MailboxAddress ("Jorge", "jorgin.miranda@gmail.com"));
-            mensage.Subject = "Teste de envio de email";
+            // mensage.From.Add (new MailboxAddress ("Jorge", "jorgemirandaneto@gmail.com"));
+            // mensage.To.Add (new MailboxAddress ("Jorge", "jorgin.miranda@gmail.com"));
+            // mensage.Subject = "Teste de envio de email";
 
-            var bodyBuilder = new BodyBuilder ();
-            bodyBuilder.HtmlBody = "<a href="+"www.google.com.br"+">Teste de link aqui.</a>";
-            bodyBuilder.TextBody = "Click aqui!!!";
+            // var bodyBuilder = new BodyBuilder ();
+            // bodyBuilder.HtmlBody = "<a href="+"www.google.com.br"+">Teste de link aqui.</a>";
+            // bodyBuilder.TextBody = "Click aqui!!!";
 
-            mensage.Body = bodyBuilder.ToMessageBody();
-            using (var client = new SmtpClient ()) {
-                client.Connect ("smtp.gmail.com", 587, false);
+            // mensage.Body = bodyBuilder.ToMessageBody();
+            // using (var client = new SmtpClient ()) {
+            //     client.Connect ("smtp.gmail.com", 587, false);
 
-                client.Authenticate("jorgemirandaneto@gmail.com", "********");
+            //     client.Authenticate("jorgemirandaneto@gmail.com", "********");
 
-                client.Send(mensage);
+            //     client.Send(mensage);
 
-                client.Disconnect(true);
-            }
+            //     client.Disconnect(true);
+            // }
         }
     }
 }
