c6e15af677c65d9431bf2dadfba7dd27c3017bbc
Jorge Miranda <jorgemirandaneto@gmail.com>
2019-06-30 22:07:57 -0300

    Atualuzação program

--- Program.cs
+++ Program.cs
@@ -8,17 +8,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Fantasy_server
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
+namespace Fantasy_Service {
+    public class Program {
+        public static void Main (string[] args) {
+            CreateWebHostBuilder (args).Build ().Run ();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseStartup<Startup> ()
+            .UseKestrel ((context, options) => {
+                var port = Environment.GetEnvironmentVariable ("PORT");
+                if (!string.IsNullOrEmpty (port)) {
+                    options.ListenAnyIP (int.Parse (port));
+                }
+            });
     }
 }
